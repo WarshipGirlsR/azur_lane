dad2342615d52d395a548023c49503197099584c
WarshipGirlsR <WarshipGirlsR>
2017-11-15 03:25:22 +0800

    no message

--- AStart.lua
+++ AStart.lua
@@ -1,11 +1,18 @@
-local function isPointInList(point, list)
-  for k = 1, #list do
-    local v = list[k]
-    if point[1] == v[1] and point[2] == v[2] then
-      return true
-    end
-  end
-  return false
+local function setPointInMap(p, map)
+  map[p[1][1] .. ',' .. p[1][2]] = p
+end
+
+local function getPointInMap(p, map)
+  return map[p[1][1] .. ',' .. p[1][2]]
+end
+
+local function pointGenerator(startPoint, endPoint, weight, lastPoint)
+  weight = weight or 1
+  local point = { startPoint[1], startPoint[2], lastPoint = lastPoint }
+  point.G = lastPoint and (lastPoint.G + weight) or 0
+  point.H = math.abs(endPoint[1] - startPoint[1]) + math.abs(endPoint[2] - startPoint[2])
+  point.F = point.G + point.H
+  return point
 end
 
 -- 找到F值最小的点，如果F值相同则取靠后的
@@ -31,36 +38,43 @@ local AStart = function(startPoint, endPoint, mapObj)
   local width = mapObj.width
   local height = mapObj.height
   -- 障碍物列表
-  local obstacle = mapObj.obstacle
+  local obstacleMap = {}
+  for key = 0, #mapObj.obstacle do
+    setPointInMap({ mapObj.obstacle[key] }, obstacleMap)
+  end
+
   -- 开列表和关列表
   local openList = {}
-  local closeList = {}
+  local openListMap = {}
+  local closeListMap = {}
 
   -- 将起始点插入开列表
-  local theStartPoint = { startPoint[1], startPoint[2] }
-  theStartPoint.G = 0
-  theStartPoint.H = math.abs(endPoint[1] - theStartPoint[1]) + math.abs(endPoint[2] - theStartPoint[2])
-  theStartPoint.F = theStartPoint.G + theStartPoint.H
-  theStartPoint.lastPoint = nil
-
+  local theStartPoint = { pointGenerator(startPoint, endPoint) }
   table.insert(openList, theStartPoint)
+  setPointInMap(theStartPoint, openList)
 
   while #openList > 0 do
     -- 从开列表选出F值最小的点，如果F值大小相同就选靠后的
     local thePoint, thePointIndex = findMinFPoint(openList)
     table.remove(openList, thePointIndex)
-    table.insert(closeList, thePoint)
+    setPointInMap(nil, openListMap)
+    setPointInMap(thePoint, closeListMap)
 
     -- 左边
     local leftPoint = { thePoint[1], thePoint[2] - 1 }
     if leftPoint[2] >= 1
-      and not isPointInList(leftPoint, closeList)
-      and not isPointInList(leftPoint, obstacle) then
-      leftPoint.G = thePoint.G + 1
-      leftPoint.H = math.abs(endPoint[1] - leftPoint[1]) + math.abs(endPoint[2] - leftPoint[2])
-      leftPoint.F = leftPoint.G + leftPoint.H
-      leftPoint.lastPoint = thePoint
-      table.insert(openList, leftPoint)
+      and not getPointInMap(leftPoint, closeListMap)
+      and not getPointInMap(leftPoint, obstacleMap) then
+      local oldOpenPoint = getPointInMap(leftPoint, openListMap)
+      local newPoint = pointGenerator(leftPoint, endPoint, 1, thePoint)
+      if oldOpenPoint then
+        if oldOpenPoint[1].F > newPoint.F then
+          oldOpenPoint[1] = newPoint
+        end
+      else
+        table.insert(openList, { newPoint })
+        setPointInMap({ newPoint }, openListMap)
+      end
       if leftPoint.H == 0 then
         break
       end
@@ -68,42 +82,36 @@ local AStart = function(startPoint, endPoint, mapObj)
     -- 右边
     local rightPoint = { thePoint[1], thePoint[2] + 1 }
     if rightPoint[2] <= width
-      and not isPointInList(rightPoint, closeList)
-      and not isPointInList(rightPoint, obstacle) then
-      rightPoint.G = thePoint.G + 1
-      rightPoint.H = math.abs(endPoint[1] - rightPoint[1]) + math.abs(endPoint[2] - rightPoint[2])
-      rightPoint.F = rightPoint.G + rightPoint.H
-      rightPoint.lastPoint = thePoint
-      table.insert(openList, rightPoint)
-      if rightPoint.H == 0 then
+      and not getPointInMap(rightPoint, closeListMap)
+      and not getPointInMap(rightPoint, obstacleMap) then
+      local newPoint = pointGenerator(rightPoint, endPoint, 1, thePoint)
+      table.insert(openList, newPoint)
+      setPointInMap(newPoint, openListMap)
+      if newPoint.H == 0 then
         break
       end
     end
     -- 上边
-    local topPoint = { thePoint[1] - 1, thePoint[2] }
-    if topPoint[1] >= 1
-      and not isPointInList(topPoint, closeList)
-      and not isPointInList(topPoint, obstacle) then
-      topPoint.G = thePoint.G + 1
-      topPoint.H = math.abs(endPoint[1] - topPoint[1]) + math.abs(endPoint[2] - topPoint[2])
-      topPoint.F = topPoint.G + topPoint.H
-      topPoint.lastPoint = thePoint
-      table.insert(openList, topPoint)
-      if topPoint.H == 0 then
+    local upPoint = { thePoint[1] - 1, thePoint[2] }
+    if upPoint[1] >= 1
+      and not getPointInMap(upPoint, closeListMap)
+      and not getPointInMap(upPoint, obstacleMap) then
+      local newPoint = pointGenerator(upPoint, endPoint, 1, thePoint)
+      table.insert(openList, newPoint)
+      setPointInMap(newPoint, openListMap)
+      if newPoint.H == 0 then
         break
       end
     end
     -- 下边
-    local bottonPoint = { thePoint[1] + 1, thePoint[2] }
-    if bottonPoint[1] <= height
-      and not isPointInList(bottonPoint, closeList)
-      and not isPointInList(bottonPoint, obstacle) then
-      bottonPoint.G = thePoint.G + 1
-      bottonPoint.H = math.abs(endPoint[1] - bottonPoint[1]) + math.abs(endPoint[2] - bottonPoint[2])
-      bottonPoint.F = bottonPoint.G + bottonPoint.H
-      bottonPoint.lastPoint = thePoint
-      table.insert(openList, bottonPoint)
-      if bottonPoint.H == 0 then
+    local downPoint = { thePoint[1] + 1, thePoint[2] }
+    if downPoint[1] <= height
+      and not getPointInMap(downPoint, closeListMap)
+      and not getPointInMap(downPoint, obstacleMap) then
+      local newPoint = pointGenerator(downPoint, endPoint, 1, thePoint)
+      table.insert(openList, newPoint)
+      setPointInMap(newPoint, openListMap)
+      if newPoint.H == 0 then
         break
       end
     end
