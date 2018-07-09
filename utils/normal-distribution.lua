d8f7620e56fcbc665d634e6f77c56345f2133395
WarshipGirlsR <fgfg163002@163.com>
2018-07-09 17:06:10 +0800

    Create normal-distribution.lua

---
+++ utils/normal-distribution.lua
@@ -0,0 +1,7 @@
+local function normalDistribution(x, u, q) {
+  u = u or 0
+  q = q or 1
+  return 1 / math.sqrt(2 * math.pi) * math.exp(0 - math.pow(x - u, 2) / 2 * math.pow(q, 2))
+}
+
+return normalDistribution
