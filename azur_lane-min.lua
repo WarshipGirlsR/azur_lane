340fdf0d33c0ea9f81c0d9e25696baef4ad8d83c
WarshipGirlsR <WarshipGirlsR>
2018-07-24 10:42:15 +0800

    no message

--- azur_lane-min.lua
+++ azur_lane-min.lua
@@ -16022,7 +16022,7 @@ local lfs = require '../lib/lfs'\
 local logPath = userPath() .. '/log'\
 local theTime = os.time()\
 \
-os.execute('find \"' .. logPath .. '\" -mtime +7 -type f -name azur_lane_*.log -exec rm -f {} \\;')\
+os.execute('find \"' .. logPath .. '\" -mtime +7 -type f -name azur_lane_*.log -exec rm -f {} \\\\;')\
 " }
 
 
